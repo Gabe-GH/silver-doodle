02eb8f98b3f727f98de698a5c26b72fc377d7e89
Gabe-GH <gabriel.lira01@utrgv.edu>
2022-03-20 19:28:45 -0500

    added middleware to allow json data to be accepted by express routes

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+// MIDDLEWARE
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 // ROUTE IMPORTS
 const professorsAPI = require('./Routes/v1_professor_routes')
 
